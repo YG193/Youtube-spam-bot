ebfccf6d8b59d582327f87173a815684bcb65a17
YG193 <yg193real@gmail.com>
2025-06-22 17:48:44 +0300

    Update youtubespambot.py

--- youtubespambot.py
+++ youtubespambot.py
@@ -6,13 +6,13 @@ import time
 browser = webdriver.Chrome('/path/to/chromedriver')
 
 # Open the YouTube live stream page
-browser.get('https://www.youtube.com/watch?v=VIDEO_ID')
+browser.get('https://www.youtube.com/watch?v=IoQK8AAXfMg')
 
 while True:
     # Type "spam"
     chat_box = browser.find_element_by_xpath('//*[@id="chat-input"]')
-    chat_box.send_keys('puan')
+    chat_box.send_keys('youbrowser23')
     chat_box.submit()
     
     # Wait for 5 minutes
-    time.sleep(300)
+    time.sleep(2)
